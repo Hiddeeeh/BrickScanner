0ca6e9ee9df589a25d6a27742279b5321e5beb51
Hiddeeeh <hidde.linderse@gmail.com>
2024-12-06 17:03:39 +0100

    Added floor data/floor builder

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -12,7 +12,17 @@ public class Wall
 }
 
 [System.Serializable]
+public class Floor
+{
+    public Vector3 corner1;
+    public Vector3 corner2;
+    public Vector3 corner3;
+    public Vector3 corner4;
+}
+
+[System.Serializable]
 public class WallData
 {
     public Wall[] walls;
+    public Floor[] floors;
 }
--- Assets/Scripts/WallGenerator.cs
+++ Assets/Scripts/WallGenerator.cs
@@ -7,11 +7,13 @@ public class WallGenerator : MonoBehaviour
     //Het doel van deze is om de Json data te parsen naar Unity.
     WallData wallData;
     float wallHeight = 5.0f;
+    float floorHeight = 0.1f;
 
     void Start()
     {
         LoadWallData();
         GenerateWall();
+        GenerateFloor();
     }
 
     void LoadWallData()
@@ -27,6 +29,8 @@ public class WallGenerator : MonoBehaviour
         {
             Debug.LogError("wallData.json not found.");
         }
+
+
     }
 
     void GenerateWall()
@@ -39,12 +43,31 @@ public class WallGenerator : MonoBehaviour
             Vector3 wallThicknessVector = (wall.corner2 + wall.corner3) / 2 - (wall.corner1 + wall.corner4) / 2;
             float wallThickness = wallThicknessVector.magnitude * 100;
             Vector3 wallCenter = (wall.corner1 + wall.corner2 + wall.corner3 + wall.corner4) / 4 * 100;
+            wallCenter.y += (wallHeight / 2);
 
             GameObject wallObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
             wallObject.transform.position = wallCenter;
             wallObject.transform.localScale = new Vector3(wallThickness, wallHeight, wallLength);
             wallObject.transform.SetParent(this.transform);
+        }
+    }
+
+    void GenerateFloor()
+    {
+        foreach (var floor in wallData.floors)
+        {
+            Vector3 floorLengthVector = (floor.corner3 + floor.corner4) / 2 - (floor.corner1 + floor.corner2) / 2;
+            float floorLength = floorLengthVector.magnitude * 100;
+            Vector3 floorThicknessVector = (floor.corner2 + floor.corner3) / 2 - (floor.corner1 + floor.corner4) / 2;
+            float floorThickness = floorThicknessVector.magnitude * 100;
+            Vector3 floorCenter = (floor.corner1 + floor.corner2 + floor.corner3 + floor.corner4) / 4 * 100;
+
+            GameObject floorObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+            floorObject.transform.position = floorCenter;
+            floorObject.transform.localScale = new Vector3(floorThickness, floorHeight, floorLength);
+            floorObject.transform.SetParent(this.transform);
         }
     }
 }
